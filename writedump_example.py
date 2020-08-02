6003220a0eb01fcc12e853b318d9828fa8766495
Sumit Lahiri <lahiri.devs@gmail.com>
2020-08-02 10:32:15 +0530

    Update writedump_example.py

--- writedump_example.py
+++ writedump_example.py
@@ -14,4 +14,20 @@ def dump_rand_ints (fileptr, start, end, length) :
         rnd_int = rnd.randint(start, end)
         fileptr.write(str(rnd_int) + "\n")
         
-dump_rand_ints(fileptr, 100, 999999999, 8192)
+dump_rand_ints(fileptr, 100, 999999999, 15000000)
+
+## Check and print Duplicates
+## Astonishingly, there are no dup upto 1000000
+
+fileptr = open("input.txt", "r")
+mapping = dict()
+
+for lines in fileptr.readlines() :
+    if lines.rstrip() in mapping :
+        mapping[lines.rstrip()] = "Duplicate"
+    else :
+        mapping[lines.rstrip()] = "First"
+        
+for elems in mapping :
+    if mapping[elems] == "Duplicate" :
+        print(elems)
