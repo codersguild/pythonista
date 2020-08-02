38840aac3fb62f6e102dcaccc61bc2b5aa73c95f
Sumit Lahiri <lahiri.devs@gmail.com>
2020-08-02 11:50:36 +0530

    Update items_example.py

--- items_example.py
+++ items_example.py
@@ -17,3 +17,15 @@ print(sorted(temp))
 
 c = {'a':10, 'b':12, 'c':22}
 print(sorted([ (v, k) for k, v in c.items() ]))
+
+## Sorted Dictionary print
+
+mapping = {
+        1 : "orb",
+        3 : "zagred",
+        4 : "walrus",
+        2 : "numbis"
+    }
+
+for k, v in sorted(mapping.items()) : 
+    print(v, str(k)) 
