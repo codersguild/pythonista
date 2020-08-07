ce9e1d6c9e48200de0a0e2cae1b5c9023853e11c
Sumit Lahiri <lahiri.devs@gmail.com>
2020-08-07 18:30:31 +0530

    Update spotify_data_gen.py

--- Machine Learning/spotify_data_gen.py
+++ Machine Learning/spotify_data_gen.py
@@ -4,16 +4,18 @@ import random as rnd
 ## Random Song List
 genre_list = [  "HipHop", "Hindi Jazz", "English Pop", "Hindi Pop", "Bhajan", 
                 "Bengali Classical", "Bengali Jazz", "English Jazz", "EDM", 
-                "Classical", "Dance Street", "Bhojpuri", "Hindi Lite", "English Lite"
+                "Classical", "Dance Street", "Bhojpuri", "Hindi Lite", "English Lite", 
+                "Indie Pop", "English Classical"
              ]
 
 ## Create a CSV for data processing and ML Training.
 with open("genre_detect.csv", mode="w") as genre :
     genre_writer = csv.writer(genre, delimiter=',')
-    for i in range(1000) :
+    for i in range(10000) :
     
-    ## Generate some data randomly. CSV list of ==> {age}, {gender}, {genre}
-        age = rnd.randint(15, 65)
+    ## Generate some data randomly. It doesn't have much of a pattern.
+    ## CSV list of ==> {age}, {gender}, {genre}
+        age = rnd.randint(15, 55)
         gender = rnd.randint(0, 1)
         index = rnd.randint(0, len(genre_list)-1)
         
