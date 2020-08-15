ae24a3bd680c885116e412df2bbff24fd868d768
Sumit Lahiri <lahiri.devs@gmail.com>
2020-08-16 00:18:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 """
+TODOs
 Task : Use Decorators to change use of sort_array()
+Task : Sleep for random time secs not exceeding 25000
 """
 from dsa.sort import *
-    
+import random
+import time
+
 num = [x for x in range(100)]
 num.reverse()
 
 sort_array(num)
+
+time.sleep(random.randint(0, 25000))
