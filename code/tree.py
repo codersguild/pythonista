d8c98b6b0f121875c6c54c52e984b0c2bf16d676
Sumit Lahiri <lahiri.devs@gmail.com>
2021-03-05 09:37:19 +0530

    Update tree.py

--- code/tree.py
+++ code/tree.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-from graphviz import Source, Graph, Digraph
+
 import uuid
+from graphviz import Source, Graph, Digraph
 
 
 def format_stmts(node):
     return "\n".join([str(elems) for elems in node.data])
 
 
-class TreeNode(dict):
+class ExecutionTreeNode(dict):
     def __init__(self, nodeId=""):
         self.left = None
         self.right = None
@@ -16,16 +17,19 @@ class TreeNode(dict):
         self.added = False
         self.id = None
         self.data = []
+        self.trueQuerySet = []
+        self.falseQuerySet = []
         self.edges = []
         dict.__init__(self, nodeId=nodeId, uid=str(uuid.uuid4()))
         self.data.append(nodeId)
 
 
-class TreeEdge:
+class ExecutionTreeEdge:
     def __init__(self, parent, child, label="execution", color="black"):
         self.parent = parent
         self.child = child
         child.parent = parent
+        self.imapdata = ""
         self.label = label
         self.data = label
         self.color = color
@@ -44,7 +48,7 @@ class TreeEdge:
         self.graph.render()
 
 
-class Tree:
+class ExecutionTree:
     def __init__(self, nodes=[]):
         self.nodes = nodes
         self.counter = 0
@@ -52,13 +56,14 @@ class Tree:
         self.edgeSet = []
         self.root = None
 
-    def save_cfg(self, name="Tree", filename="sample", directory="."):
+    def save_cfg(self, name="ExecutionTree", filename="sample", directory="."):
         graph = Digraph(name=name,
                         filename=filename,
                         directory=directory,
                         format="png")
         for node in self.nodes:
             for edge in node.edges:
+                edge.label += edge.imapdata
                 graph.edge(
                     format_stmts(edge.parent),
                     format_stmts(edge.child),
@@ -74,3 +79,34 @@ class Tree:
             node.added = True
             node.uid = self.counter
             self.nodes.append(node)
+
+
+if __name__ == "__main__":
+    print("Building Execution Tree")
+    Tree = ExecutionTree()
+    root = ExecutionTreeNode("Root", 0)
+    arr = [1, 2, 3, 4]
+    for index, elems in enumerate(arr):
+        if index < len(arr) / 2:
+            node = ExecutionTreeNode(index)
+            left = ExecutionTreeNode(2 * index + 1)
+            right = ExecutionTreeNode(2 * index + 2)
+            Tree.add_node(node)
+            Tree.add_node(left)
+            Tree.add_node(right)
+            trueExpr = "True"
+            falseExpr = "False"
+            node.edges.append(
+                ExecutionTreeEdge(node,
+                                  left,
+                                  label=' '.join(trueExpr.split()),
+                                  color="green"))
+            node.edges.append(
+                ExecutionTreeEdge(node,
+                                  right,
+                                  label=' '.join(falseExpr.split()),
+                                  color="Red"))
+        else:
+            pass
+
+    Tree.save_cfg()
